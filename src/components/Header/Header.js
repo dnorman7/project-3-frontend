e9d685ff695967f020d10641c1321bf1bddf59d5
Deanna Norman <deannaleenorman@msn.com>
2020-12-08 12:25:58 -0800

    adds nav

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+
+export default function Header(props) {
+    return (
+        <header className='Header'>
+            <h1>Exercise App</h1>
+            <nav>
+                <ul className='NavLinks'>
+                    <li>Logout</li>
+                    <li>Dashboard</li>
+                    <li>Signup</li>
+                    <li>Login</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
